c7d15a805759eb0290a12f6e7c420563e220c4eb
Tomer Klein <tomer.klein@gmail.com>
2021-03-17 11:54:30 +0200

    Update app.py
    Adding Video Support

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ from pathlib import Path
 
 # Set artistik model url - to download if not exixts
 artistic_model_url = "https://data.deepai.org/deoldify/ColorizeArtistic_gen.pth"
+video_model_url = "https://data.deepai.org/deoldify/ColorizeVideo_gen.pth"
+
 
 # Handle switch between GPU and CPU
 if torch.cuda.is_available():
@@ -42,6 +44,9 @@ create_directory(model_directory)
 get_model_bin(artistic_model_url, os.path.join(model_directory, "ColorizeArtistic_gen.pth"))
 image_colorizer = get_image_colorizer(artistic=True)
 
+get_model_bin(video_model_url, os.path.join(model_directory, "ColorizeVideo_gen.pth"))
+video_colorizer = get_video_colorizer()
+video_colorizer.result_folder = Path(results_img_directory)
 
 
 def color(file_path,chat_id):
@@ -77,6 +82,30 @@ def color(file_path,chat_id):
     if os.path.exists(output_path):
         logger.info("Removing colorized photo from storage")
         os.remove(output_path)
+
+
+def video_color(file_path,chat_id):
+    # Coloring Video
+    logger.info("Coloring Video....")
+    output_path= video_colorizer.colorize_from_file_name(file_path, render_factor=int(render_factor))
+    # output_path = os.path.join(results_img_directory, os.path.basename(file_path))
+    #Send the colorized image back to user
+    video_data = open(output_path,"rb")
+    logger.info("Sending colorized video back to user")
+    bot.sendVideo(chat_id,video_data)
+    
+    # Delete the original photo in order to save storage
+    if os.path.exists(file_path):
+        logger.info("Removing original photo from storage")
+        os.remove(input_path)
+    
+    # Delete the colorized photo in order to save storage
+    if os.path.exists(output_path):
+        logger.info("Removing colorized photo from storage")
+        os.remove(output_path)
+
+
+
  
     
 def handle(msg):
@@ -105,6 +134,27 @@ def handle(msg):
            
     except Exception as e:
         logger.error(str(e))
+
+
+
+    try:
+        # Check if the mesaage contains photo (Compressed)
+        if 'video' in str(msg):
+            logger.info('============ Video Recived ====================')
+            if 'document' in str(msg):
+                file_id = msg['document']['file_id']
+            else:
+                file_id = msg['video']['file_id']
+            file_path = os.path.join(upload_directory,file_id+".mp4")
+            bot.download_file(file_id,file_path)
+            bot.sendMessage(chat_id,"Coloring, please wait...")
+            video_color(file_path,chat_id)
+    except Exception as e:
+        logger.error(str(e))
+
+
+
+
 # Starts the bot
 bot = telepot.Bot(BOT_TOKEN)
 MessageLoop(bot, handle).run_as_thread()
